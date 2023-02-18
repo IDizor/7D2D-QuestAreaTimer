de9cfddec82da7a0df2fa5233c62446201784685
IDizor <idizorr@gmail.com>
2023-02-18 12:03:39 +0200

    Version 1.2

--- QuestAreaTimer/Properties/AssemblyInfo.cs
+++ QuestAreaTimer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QuestAreaTimer")]
-[assembly: AssemblyDescription("7 Days to Die game modlet: When player leaves the quest area - creates a return-timer instead of an instant quest failure.")]
+[assembly: AssemblyDescription("7 Days to Die game modlet: When player leaves the quest area - displays a return-timer instead of an instant quest failure.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IDizor")]
 [assembly: AssemblyProduct("QuestAreaTimer")]
-[assembly: AssemblyCopyright("Copyright © IDizor 2022")]
+[assembly: AssemblyCopyright("Copyright © IDizor 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2")]
